22c914abff39419f698dd0527609606157b95925
tuandev <tuanitdev@gmail.com>
2020-05-28 15:51:39 +0700

    init app

---
+++ app/src/main/java/com/quan/hineighboorguide2/Movie.java
@@ -0,0 +1,30 @@
+package com.quan.hineighboorguide2;
+
+public class Movie {
+    private String title;
+    private int image;
+
+    public Movie() {
+    }
+
+    public Movie(String title,int image ){
+        this.title = title;
+        this.image = image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String name) {
+        this.title = name;
+    }
+
+    public int getImage() {
+        return image ;
+    }
+
+    public void setImage(String genre) {
+        this.image = image;
+    }
+}
